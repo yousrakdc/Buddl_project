decd5830ce144c0122a88a68e610b6022f29ce62
Yousra <142456388+yousrakdc@users.noreply.github.com>
2025-02-13 12:57:50 +0100

    config

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        pathRewrite: { '^/data': '' }
+      }
+    }
+  }
+})
